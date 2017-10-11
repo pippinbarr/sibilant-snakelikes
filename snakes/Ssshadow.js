49651309659a806add1a3174259ae8797f3f1657
Pippin Barr <pippin.barr@gmail.com>
2017-10-11 19:11:51 -0400

    +Tracking apple, +Eating apple in locations, +Death on colossus collision
    +Tracking apple: Added code so that we know where the apple currently is relative to the rest of the colossus's body. Might conceivably be redundant? Not sure now that I think about it.
    
    + You can now eat the apple and it will respawn at the next location on the colossus (and the previous bit of the colossus will be replace with a solid brick - currently this is screwing things up because the snake is dying when it eats an apple).
    
    + You can now die if you hit the colossus with the snake head. Which is appropriate. Death comes for us all.
    
    ? One option for the death-on-impact is that when the snake 'eats' one of these apples it's just gone forever. So it eats the climbing grip on the colossus. I guess that makes a small degree of sense - it would be a case of snake rules trumping colossus rules. I'm tempted by it because it's obviously easier.
    
    + Fuck it, I'm going to make that change now. So now an eating grip/apple on the colossus is just gone, no more body there.
    
    - At which point we don't need to track the location of the apple in relation to the data. So I removed that. I guess it just will be as if it never happened.

--- snakes/Ssshadow.js
+++ snakes/Ssshadow.js
@@ -8,8 +8,11 @@ BasicGame.Ssshadow.prototype.constructor = BasicGame.Ssshadow;
 BasicGame.Ssshadow.prototype.create = function () {
   BasicGame.Snake.prototype.create.call(this);
 
+  // Data to represent the colossus by creating it out of tiles
+  // This will let me specify where the target points are so that they
+  // can be "apples" at the right time, too.
   this.colossusData = [
-    [0,0,0,0,1,0,0,0,0],
+    [0,0,0,0,6,0,0,0,0],
     [0,0,0,0,1,0,0,0,0],
     [0,0,0,1,1,1,0,0,0],
     [0,1,1,1,1,1,1,1,0],
@@ -17,32 +20,50 @@ BasicGame.Ssshadow.prototype.create = function () {
     [1,0,1,1,1,1,1,0,1],
     [1,0,0,1,1,1,0,0,1],
     [1,0,0,0,1,0,0,0,1],
-    [1,0,0,0,1,0,0,0,1],
+    [4,0,0,0,1,0,0,0,5],
     [0,0,0,1,1,1,0,0,0],
     [0,0,0,1,0,1,0,0,0],
     [0,0,0,1,0,1,0,0,0],
     [0,0,0,1,0,1,0,0,0],
     [0,0,0,1,0,1,0,0,0],
-    [0,0,2,1,0,1,1,0,0]
+    [0,0,2,1,0,1,3,0,0]
   ];
+  this.colossusBits = [];
+  this.FINAL_APPLE_INDEX = 6;
+  this.currentAppleIndex = 2;
 
-  this.colossusGroup = this.game.add.group();
+  // The colossus will be represented as a group
+  this.colossus = this.game.add.group();
+
+  // Go through the data for the colossus and add the appropriate tiles
+  // in the appropriate locations to the colossusGroup
   for (var y = 0; y < this.colossusData.length; y++) {
+    this.colossusBits.push([]);
     for (var x = 0; x < this.colossusData[y].length; x++) {
-      if (this.colossusData[y][x] == 1) {
-        var bit = this.game.add.sprite(x * this.GRID_SIZE,y*this.GRID_SIZE,'body');
-        this.colossusGroup.add(bit);
+      var bit = null;
+      if (this.colossusData[y][x] == 2) {
+        this.apple.x = x * this.GRID_SIZE;
+        this.apple.y = y * this.GRID_SIZE,'apple';
+        this.colossus.add(this.apple);
       }
-      else if (this.colossusData[y][x] == 2) {
-        var bit = this.game.add.sprite(x * this.GRID_SIZE,y*this.GRID_SIZE,'apple');
-        this.colossusGroup.add(bit);
+      else if (this.colossusData[y][x] > 0) {
+        bit = this.game.add.sprite(x * this.GRID_SIZE,y*this.GRID_SIZE,'body');
+        this.colossusBits[y][x] = bit;
       }
+
+      if (bit) {
+        this.colossus.add(bit);
+      }
+
+      this.colossusBits[y].push(bit);
     }
   }
 
-  this.colossusGroup.x = 12 * this.GRID_SIZE;
-  this.colossusGroup.y = 12 * this.GRID_SIZE;
+  // Move the entire colossus group to a starting position
+  this.colossus.x = 12 * this.GRID_SIZE;
+  this.colossus.y = 12 * this.GRID_SIZE;
 
+  // Name the state for resetting purposes
   this.stateName = "Ssshadow";
 };
 
@@ -57,14 +78,30 @@ BasicGame.Ssshadow.prototype.tick = function () {
 }
 
 BasicGame.Ssshadow.prototype.checkAppleCollision = function () {
-  if (this.snakeHead.position.equals(this.apple.position)) {
-
+  if (this.snakeHead.position.equals(this.apple.world)) {
+    this.appleSFX.play();
+    this.currentAppleIndex++;
+    if (this.currentAppleIndex > this.MAX_APPLE_INDEX) {
+      // That was the final apple!
+      console.log("You defeated the colossus!")
+    }
+    else {
+      for (var y = 0; y < this.colossusData.length; y++) {
+        for (var x = 0; x < this.colossusData[y].length; x++) {
+          if (this.colossusData[y][x] == this.currentAppleIndex) {
+            this.colossusBits[y][x].destroy();
+            this.apple.position.x = (x * this.GRID_SIZE);
+            this.apple.position.y = (y * this.GRID_SIZE);
+          }
+        }
+      }
+    }
   }
 };
 
 BasicGame.Ssshadow.prototype.checkColossusCollision = function () {
-  this.snakeBodyGroup.forEach(function (bit) {
-    if (this.snakeHead.position.equals(bit.position)) {
+  this.colossus.forEach(function (bit) {
+    if (this.snakeHead.position.equals(bit.world)) {
       this.die();
       return;
     }
@@ -78,4 +115,9 @@ BasicGame.Ssshadow.prototype.gameOver = function () {
   else {
     this.setGameOverText("GAME OVER","",this.score + " POINTS","","");
   }
+};
+
+
+BasicGame.Ssshadow.prototype.repositionApple = function () {
+
 };
