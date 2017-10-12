ccd9b420344049c1dd8b6c26277ef61af1d19253
Pippin Barr <pippin.barr@gmail.com>
2017-10-12 19:53:01 -0400

    Add commenting
    Added comments to the code. In part because it's the right thing to do, but also in part after discussion about the research-creation method in directed study with Rilla, Cody, and Tony. Notably the idea of technical documentation being an important component of a project. Especially if this is meant to be some kind of 'open source' it's ridiculous to not comment what the code does for someone who might want to read it.
    
    Should probably look into javadoc or some sort of documentation generation setup? Although it's not like I'm writing a damn API...

--- snakes/Ssshadow.js
+++ snakes/Ssshadow.js
@@ -28,7 +28,12 @@ BasicGame.Ssshadow.prototype.create = function () {
     [0,0,0,1,0,1,0,0,0],
     [0,0,2,1,0,1,3,0,0]
   ];
+
+  // An array to store a representation of the colossus matching the data
+  // but with the actual tiles in it
   this.colossusBits = [];
+
+  // Tracking which apple we're up to
   this.FINAL_APPLE_INDEX = 6;
   this.currentAppleIndex = 2;
   this.currentAppleBitPosition = {
@@ -46,21 +51,27 @@ BasicGame.Ssshadow.prototype.create = function () {
     for (var x = 0; x < this.colossusData[y].length; x++) {
       var bit = null;
       if (this.colossusData[y][x] == 2) {
+        // If this is the first apple, then add an apple at that location
         this.apple.x = x * this.GRID_SIZE;
         this.apple.y = y * this.GRID_SIZE,'apple';
         this.colossus.add(this.apple);
+        // Track the apple
         this.currentAppleBitPosition.x = x;
         this.currentAppleBitPosition.y = y;
+        // We don't add the apple to colossus bits, since it's temporary
       }
       else if (this.colossusData[y][x] > 0) {
+        // If this is either a bit of body or a future apple site, add a body tile
         bit = this.game.add.sprite(x * this.GRID_SIZE,y*this.GRID_SIZE,'body');
         this.colossusBits[y][x] = bit;
       }
 
+      // If a bit was created, we add it to the main group that makes up the colossus
       if (bit) {
         this.colossus.add(bit);
       }
 
+      // Add this bit (even if null) to the 2D array representation of the colossus
       this.colossusBits[y].push(bit);
     }
   }
@@ -80,29 +91,49 @@ BasicGame.Ssshadow.prototype.update = function () {
 BasicGame.Ssshadow.prototype.tick = function () {
   BasicGame.Snake.prototype.tick.call(this);
 
+  this.colossusMove();
   this.checkColossusCollision();
 }
 
+BasicGame.Ssshadow.prototype.colossusMove() = function () {
+
+};
+
 BasicGame.Ssshadow.prototype.checkAppleCollision = function () {
+  // Check if the snake's head is over the apple
+  // We use world position because the apple is in a group
   if (this.snakeHead.position.equals(this.apple.world)) {
+    // Play the sound
     this.appleSFX.play();
+    // Create a replace bit so that after the apple gets eaten the colossus
+    // has a body tile at that location from then on
     var replacementBit = this.game.add.sprite(this.apple.x,this.apple.y,'body');
     this.colossus.add(replacementBit);
     this.colossusBits[this.currentAppleBitPosition.y][this.currentAppleBitPosition.x] = replacementBit;
+
+    // Increment the apple index
     this.currentAppleIndex++;
+    // Check if that was the final apple or not
     if (this.currentAppleIndex > this.MAX_APPLE_INDEX) {
       // That was the final apple!
       console.log("You defeated the colossus!")
     }
     else {
+      // If wasn't the final apple so we go through the colossus data to find
+      // the the location of the nexxt apple
       for (var y = 0; y < this.colossusData.length; y++) {
         for (var x = 0; x < this.colossusData[y].length; x++) {
           if (this.colossusData[y][x] == this.currentAppleIndex) {
+            // Get rid of the tile at that location
             this.colossusBits[y][x].destroy();
+            // Move the apple to that location
             this.apple.position.x = (x * this.GRID_SIZE);
             this.apple.position.y = (y * this.GRID_SIZE);
+            // Store the location in the colossusBits of the apple
             this.currentAppleBitPosition.x = x;
             this.currentAppleBitPosition.y = y;
+            // Having positioned the apple, we can exit
+            return;
           }
         }
       }
@@ -111,8 +142,11 @@ BasicGame.Ssshadow.prototype.checkAppleCollision = function () {
 };
 
 BasicGame.Ssshadow.prototype.checkColossusCollision = function () {
+  // Go through all the colossus elements
   this.colossus.forEach(function (bit) {
+    // Check if there's a collision was with a body tile (not the apple)
     if (this.snakeHead.position.equals(bit.world) && bit != this.apple) {
+      // Death if so
       this.die();
       return;
     }
@@ -120,15 +154,11 @@ BasicGame.Ssshadow.prototype.checkColossusCollision = function () {
 };
 
 BasicGame.Ssshadow.prototype.gameOver = function () {
-  if (this.ateApple) {
-    this.setGameOverText("GAME OVER","","YOU LOSE","","");
-  }
-  else {
-    this.setGameOverText("GAME OVER","",this.score + " POINTS","","");
-  }
+
 };
 
 
 BasicGame.Ssshadow.prototype.repositionApple = function () {
-
+  // We don't reposition the apple with the usual algorithm
+  // because it's based on the data about the colossus
 };
